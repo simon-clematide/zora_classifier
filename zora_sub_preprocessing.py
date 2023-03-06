d20da721261e5ce6f7efb3cd8097ff8f7926699f
Jessica <jessica.roady@uzh.ch>
2023-03-06 18:01:19 +0100

    Continue retracing workflow in .ipynb - checking for missing corresponding data.

--- zora_sub_preprocessing.py
+++ zora_sub_preprocessing.py
@@ -4,48 +4,64 @@
 
 from langdetect import detect
 
-with open("data/robin_data.tsv") as f:
-    rows = []
-    rows_before_rstrip = []
-    rows_after_rstrip = []
-    rows_after_tsplit = []
-
-    for line in f:
-        rows_before_rstrip.append(line)
-        line = line.rstrip("\n")
-        rows_after_rstrip.append(line)
-        row = line.split("\t")
-        rows_after_tsplit.append(row)
-
-        # TODO: Make row IDs
-
-        # Only those with abstracts
-        if row[-1] != "":
-            # TODO: Find the one that's just a quote mark
-            if row[-1] == "\"":
-                print(row)
-                # print(num_after_tsplit)
-            else:
-                if detect(row[-1]) == "en":
-                    rows.append(row)
-
-rows.pop(0)
-
-fuck_ups = 0
-for i in rows:
-    if len(i) != 7:
-        fuck_ups += 1
-        print(i)
-
-print(f"Num. rows: {len(rows)}")
-print(f"Num. fuck-ups: {fuck_ups}")
-print(f"Num. non-fuck-ups: {len(rows) - fuck_ups}")
-
-print(len(rows_before_rstrip))
-print(len(rows_after_rstrip))
-print(len(rows_after_tsplit))
-
-# The problem is when there's a newline in the abstract
+# # OLD
+# with open("data/robin_data.tsv") as f:
+#     rows = []
+#     rows_before_rstrip = []
+#     rows_after_rstrip = []
+#     rows_after_tsplit = []
+#
+#     for line in f:
+#         rows_before_rstrip.append(line)
+#         line = line.rstrip("\n")
+#         rows_after_rstrip.append(line)
+#         row = line.split("\t")
+#         rows_after_tsplit.append(row)
+#
+#         # Only those with abstracts
+#         if row[-1] != "":
+#             if row[-1] == "\"":
+#                 print(row)
+#                 # print(num_after_tsplit)
+#             else:
+#                 if detect(row[-1]) == "en":
+#                     rows.append(row)
+#
+# rows.pop(0)
+#
+# fuck_ups = 0
+# for i in rows:
+#     if len(i) != 7:
+#         fuck_ups += 1
+#         print(i)
+#
+# print(f"Num. rows: {len(rows)}")
+# print(f"Num. fuck-ups: {fuck_ups}")
+# print(f"Num. non-fuck-ups: {len(rows) - fuck_ups}")
+#
+# print(len(rows_before_rstrip))
+# print(len(rows_after_rstrip))
+# print(len(rows_after_tsplit))
 
 # for i in rows:
 #     print(i)
+
+with open("data/robin_data.tsv") as f:
+    rows = []
+    row_id = 0
+
+    for line in f:
+        line = line.rstrip("\n")
+        row = line.split("\t")
+
+        # Only those with abstracts
+        if row[-1] != "" and row[-1] != "\"" and detect(row[-1]) == "en":
+            row_id += 1
+            row.insert(0, row_id)
+            rows.append(row)
+
+# buggy_rows = [i for i in rows if len(i) != 8]
+# print([i for i in buggy_rows])
+# print(len(buggy_rows))
+
+print(len(rows[0]))
